b2bba1f35a2eca40947ab3a9bc701ce91749f297
Stefan Kendall <stefankendall@gmail.com>
2012-01-15 17:31:58 -0500

    Fixing starting port

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
+}).listen(port, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
