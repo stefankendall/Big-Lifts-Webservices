75b9e6709ca1f3928f78b81458c76a9abc621b68
Stefan Kendall <stefankendall@gmail.com>
2012-01-15 17:34:10 -0500

    Once again attempting to fix node deploy

--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(port, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:1337/');
+}).listen(port, "0.0.0.0");
+console.log('Server running on port ' + port);
