0770483c5b2938165a8db89d7a401c334301ded5
skendall <skendall@skendalllaptop.(none)>
2012-01-31 23:38:54 -0500

    Added logging to see what mongo is connecting to

--- routes/lib/mongo.js
+++ routes/lib/mongo.js
@@ -1,3 +1,5 @@
 var Mongolian = require("mongolian");
+var util = require('util');
 var mongoHqUrl = process.env['MONGOHQ_URL'] || 'mongodb://localhost/wendler';
+util.log( "Connecting to mongo: " + mongoHqUrl );
 exports.db = new Mongolian(mongoHqUrl);
