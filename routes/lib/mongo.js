5ad32ba8562287d00a1e923a2223bc2be7840a7a
skendall <skendall@skendalllaptop.(none)>
2012-01-31 23:45:13 -0500

    Removed redundant logging

--- routes/lib/mongo.js
+++ routes/lib/mongo.js
@@ -1,5 +1,3 @@
 var Mongolian = require("mongolian");
-var util = require('util');
 var mongoHqUrl = process.env['MONGOHQ_URL'] || 'mongodb://localhost/wendler';
-util.log( "Connecting to mongo: " + mongoHqUrl );
 exports.db = new Mongolian(mongoHqUrl);
