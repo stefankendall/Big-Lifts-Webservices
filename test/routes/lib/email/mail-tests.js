7c1576ecc731c2feb92baa1d766eea2968e9cfd1
skendall <skendall@skendalllaptop.(none)>
2012-02-20 13:32:26 -0500

    Tests pass for sending email - then commented out. Need to only run these sporadically until I have more email credits.

--- test/routes/lib/email/mail-tests.js
+++ test/routes/lib/email/mail-tests.js
@@ -1,20 +1,50 @@
 var vows = require('vows'),
     assert = require('assert');
 
-var mailer = require('../../../../routes/lib/email/mail.js').mailer;
+var postageapp = require('../../../../routes/lib/email/mail.js').postageapp;
 
 vows.describe('sending email').addBatch({
-    'Send empty email':{
-        topic:function () {
-            mailer.send_mail({
-                sender:'wendler531@stefankendall.com',
-                to: 'stefankendall@gmail.com',
-                subject: "Vows test",
-                body: 'Test body'
-            }, this.callback)
-        },
-        'sends successfully':function (error, response) {
-            assert.include(error.message, '200')
-        }
-    }
+//    'plaintext':{
+//        topic:function () {
+//            var options = {
+//                recipients:'stefankendall@gmail.com',
+//                subject:'Subject Line',
+//                from:'export@wendler.mobi',
+//                content:{
+//                    'text/plain':"Hello, World!"
+//                }
+//            };
+//            postageapp.sendMessage(options, this.callback);
+//        },
+//        'sends successfully':function (error, response) {
+//            console.log(response);
+//            assert.equal(JSON.parse(response).response.status, "ok");
+//        }
+//    },
+//    'with attachment':{
+//        topic:function () {
+//            var attachmentData = "date,weight,reps\n" +
+//                "02/02/2011,145,6\n" +
+//                "02/04/2011,155,5\n";
+//            var options = {
+//                recipients:'stefankendall@gmail.com',
+//                subject:'Subject Line',
+//                from:'export@wendler.mobi',
+//                content:{
+//                    'text/plain':"File attached."
+//                },
+//                attachments:{
+//                    'log.csv':{
+//                        'content_type':'text/csv',
+//                        'content':(new Buffer(attachmentData)).toString("base64")
+//                    }
+//                }
+//            };
+//            postageapp.sendMessage(options, this.callback);
+//        },
+//        'sends successfully':function (error, response) {
+//            console.log(response);
+//            assert.equal(JSON.parse(response).response.status, "ok");
+//        }
+//    }
 }).export(module);
